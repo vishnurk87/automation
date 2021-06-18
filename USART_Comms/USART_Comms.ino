d303ca6af27af099308e80c48aedd3bcf1342c50
Vishnu <vishnurk879@gmail.com>
2021-06-18 15:23:01 -0400

    implement tx and rx of a byte of data

--- USART_Comms/USART_Comms.ino
+++ USART_Comms/USART_Comms.ino
@@ -24,27 +24,33 @@
 volatile uint8_t USART_TX_Buffer; // Global Buffer
 volatile uint8_t USART_RX_DATA; // Global RX
 
+volatile int TX_shuffle = 1; // 1 for transmit and 0 for recieve
+
 void setup()
 {	
 	
 	DDRB |= BIT(DDB5);
+	
+	unsigned int BAUD_Prescalar = 207;
+	USART_init(BAUD_Prescalar);
+
+	//give delay here to produce error free tx and rx, otherwise tw bytes were sent instread of one
+	delay(1000);
+
+	//calling usart start function 
+	USART();
+
+}
+
+// function that sends and recieves one byte of data at a time
+void USART(){
+
 	uint8_t data[3];
 	data[0] = 'a';
 	data[1] = 'k';
 	data[2] = 'b';
-	unsigned int BAUD_Prescalar = 207;
-	USART_init(BAUD_Prescalar);
-	//for (int i = 0; i<3; i++) 
-	//{
-
-	//give delay here to produce error free tx and rx, otherwise tw bytes were sent instread of one
-	delay(1000);
-	//USART_TransmitInterrupt(data[0]);
-	USART_Recieve();
-	//}
-	
+	DR_Empty_Interrupt(data[0]);
 }
-
 
 
 void USART_Recieve()
@@ -55,8 +61,11 @@ void USART_Recieve()
 
 }
 
-void USART_TransmitInterrupt(uint8_t buffer)
+void DR_Empty_Interrupt(uint8_t buffer)
 {	
+
+	//shufle changed to TX mode (TX_shuffle =1)
+	TX_shuffle = 1;
 	USART_TX_Buffer = buffer;
 	UCSR0A |= UDRE0;
 	UCSR0B|= DATA_REG_EMPTY_INTERUPT;
@@ -95,15 +104,47 @@ void USART_init(unsigned int baud_rate)
 
 ISR(USART_UDRE_vect)
 {	
+
 	PORTB &= ~BIT(PORTB5);
 	UDR0 = USART_TX_Buffer;
-	UCSR0B &= ~DATA_REG_EMPTY_INTERUPT; // Disables the Interrupt, uncomment for one time transmission of data
+
+	// Disables the Interrupt, uncomment for one time transmission of data
+	UCSR0B &= ~DATA_REG_EMPTY_INTERUPT; 
+
+
+	//this creates an interupt when tx is complete
+	UCSR0B |= TX_COMPLETE_INTERUPT_ON;
 	//delay(1000);
 }
 
 
+//transmit complete interupt
+ISR(USART_TX_vect)
+{	
+
+	//shufle changed to RX mode (TX_shuffle =0) after writing data into transmit buffer
+	TX_shuffle = 0;
+
+	//Transmit complete interupt turned off cause now it would be in recieve mode
+	UCSR0B &= ~TX_COMPLETE_INTERUPT_ON;
+
+	// Enables data empty interupt to check if no data is there in RX buffer
+	UCSR0B |= DATA_REG_EMPTY_INTERUPT; 
+
+	// this turns on RX flag which will cause an interupt when there is unread data 
+	//in the recieve buffer
+	USART_Recieve();
+
+
+
+}
+
+
+//recieve complete interrupt
 ISR(USART_RX_vect)
-{
+{	
+	// Disables data empty interupt enabled in TX interupt and in the next lines reads the data
+	UCSR0B &= ~DATA_REG_EMPTY_INTERUPT; 
 
 	USART_RX_DATA = UDR0;
 	if (USART_RX_DATA == 'k')
@@ -115,6 +156,9 @@ ISR(USART_RX_vect)
 		PORTB &= ~BIT(PORTB5); // Writing LOW
 	}
 
+	//disabls rx ready interupt.
+	UCSR0B &= ~RX_READY_INTERUPT;
+	USART();
 }
 
 void loop()
